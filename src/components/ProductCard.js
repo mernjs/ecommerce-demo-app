14807f0b9c7d45c530713904df2472ec8b0f09b5
MernJs Community <mernjscommunity@gmail.com>
2023-06-03 22:51:13 +0530

    component breakdown

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,10 +4,11 @@ import StarRating from './StarRating';
 const ProductCard = ({ item }) => {
     return <>
         <div style={{height:"200px",width:"200px",background:"green",float:"left"}}>
-        <img src={item.thumbnail} style={{height:"200px",width:"200px"}}/> 
+            <img src={item.thumbnail} style={{height:"200px",width:"200px"}}/> 
         </div>
+
         <div style={{height:"200px",width:"400px",float:"left"}}>
-        <h2>{item.brand}</h2>
+            <h2>{item.brand}</h2>
             <b>{item.category}</b><br/>
             <b>
                 <StarRating 
@@ -15,8 +16,8 @@ const ProductCard = ({ item }) => {
                     ratedColor="#f5a440"
                 />
             </b> <br/>
-            <b>Price : {item.price}</b><br/>
-            <b>Discount : {item.discountPercentage}</b><br/>
+            <b>{item.price}</b><br/>
+            <b>{item.discountPercentage}</b><br/>
             <b>{item.description}</b>
         </div>
     </>
